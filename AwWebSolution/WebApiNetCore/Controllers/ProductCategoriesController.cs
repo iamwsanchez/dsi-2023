26620afdf47c0086df68c66ddfb5f311c4936c95
William José Sánchez Gutiérrez <williamjsg@gmail.com>
2023-03-27 14:02:27 -0600

    Se incluye ejemplo de creacion de webapi haciendo uso de scaffolding

---
+++ AwWebSolution/WebApiNetCore/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiNetCore.Db;
+
+namespace WebApiNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoriesController : ControllerBase
+    {
+        private readonly AdventureWorksContext _context;
+
+        public ProductCategoriesController(AdventureWorksContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Devuelve el listado de categorías desde la base de datos
+        /// </summary>
+        /// <returns>ProductCategory<see cref="ProductCategory"/></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategories()
+        {
+          if (_context.ProductCategories == null)
+          {
+              return NotFound();
+          }
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        /// <summary>
+        /// Devuelve los datos de la categoría indicada
+        /// </summary>
+        /// <param name="id">Identificador único de la categoría</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductCategory>> GetProductCategory(int id)
+        {
+          if (_context.ProductCategories == null)
+          {
+              return NotFound();
+          }
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return productCategory;
+        }
+
+        // PUT: api/ProductCategories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductCategory(int id, ProductCategory productCategory)
+        {
+            if (id != productCategory.ProductCategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productCategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductCategories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductCategory>> PostProductCategory(ProductCategory productCategory)
+        {
+          if (_context.ProductCategories == null)
+          {
+              return Problem("Entity set 'AdventureWorksContext.ProductCategories'  is null.");
+          }
+            _context.ProductCategories.Add(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductCategory", new { id = productCategory.ProductCategoryId }, productCategory);
+        }
+
+        // DELETE: api/ProductCategories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductCategory(int id)
+        {
+            if (_context.ProductCategories == null)
+            {
+                return NotFound();
+            }
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductCategories.Remove(productCategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+            return (_context.ProductCategories?.Any(e => e.ProductCategoryId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ AwWebSolution/WebApiNetCore/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using WebApiNetCore.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AwConnectionString");
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AdventureWorksContext>(options =>
+    options.UseSqlServer(connectionString));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//Configuración para documentación de web api
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdventureWorks WebApi", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+    c.CustomSchemaIds(type => type.ToString());
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(x=> x.DocumentTitle = "AdventureWorks WebApi");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
