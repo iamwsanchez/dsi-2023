0749c95787aa4876dfc1fe64544368a8a4c3de04
William Sanchez <williamjsg@gmail.com>
2023-05-20 09:04:27 -0600

    Se crea ejemplo de webapi con programacion en capas

--- WebApiAwLayers/WebApiAwLayers/Program.cs
+++ WebApiAwLayers/WebApiAwLayers/Program.cs
@@ -1,18 +1,19 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
-using WebApiNetCore.Db;
+using WebApiAwLayers.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("AwConnectionString");
+
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AdventureWorksContext>(options =>
-    options.UseSqlServer(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 //Configuración para documentación de web api
 builder.Services.AddSwaggerGen(c =>
 {
@@ -23,18 +24,21 @@ builder.Services.AddSwaggerGen(c =>
     c.CustomSchemaIds(type => type.ToString());
     c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 });
+builder.Services.AddBll();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI(x=> x.DocumentTitle = "AdventureWorks WebApi");
+app.UseSwagger();
+app.UseSwaggerUI(x => x.DocumentTitle = "AdventureWorks WebApi With Layers");
 //}
+
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
